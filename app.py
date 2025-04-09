898626d8ce63f1337d00f3b4e0e42e6a8b2ce543
PropClaimsPro <jaredshearmanesq@yahoo.com>
2025-04-09 15:23:15 -0500

    app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "✅ SkyVault Deployed Successfully (Flask Server Live)"
+
+@app.route('/test')
+def test():
+    return "Test route active"
